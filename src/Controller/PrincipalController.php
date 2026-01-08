5b2281172ead42c3efaa64f3be9d2015fdd85c6e
Mikel Fabricio Echeverria Jimenez <mikel140805@gmail.com>
2026-01-08 22:00:06 +0100

    portal dsps de iniciar sesion

--- src/Controller/PrincipalController.php
+++ src/Controller/PrincipalController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Producto;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +11,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class PrincipalController extends AbstractController
 {
     #[Route('/portal', name: 'portal_principal')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
-        return $this->render('portal.html.twig');
+        // ✅ 6 productos para "Continuar comprando"
+        $destacados = $em->getRepository(Producto::class)->findBy([], ['id' => 'DESC'], 6);
+
+        return $this->render('portal.html.twig', [
+            'destacados' => $destacados,
+        ]);
     }
 }
