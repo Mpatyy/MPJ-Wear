c508e45aff2f1822a4ab1ab138b969d060614331
Mikel Fabricio Echeverria Jimenez <mikel140805@gmail.com>
2026-01-08 22:59:36 +0100

    no me toqueis nada gracias

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductosRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(ProductosRepository $repo): Response
     {
-        return $this->render('home/index.html.twig');
+        // ✅ “Destacados”: últimos productos (ajusta el número si quieres)
+        $destacados = $repo->findBy([], ['id' => 'DESC'], 8);
+
+        return $this->render('home/index.html.twig', [
+            'destacados' => $destacados,
+        ]);
     }
 }
